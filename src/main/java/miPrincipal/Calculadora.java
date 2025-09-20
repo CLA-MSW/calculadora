6a84863c112cdf3623c2e9fc0a61254565475bec
Ricardo Rafael Quintero Meza, PhD <84739791+ricardo-qm@users.noreply.github.com>
2025-09-20 13:03:30 -0700

    Change arithmetic methods to return 0
    Modified all arithmetic methods to return 0 instead of the correct result.

--- src/main/java/miPrincipal/Calculadora.java
+++ src/main/java/miPrincipal/Calculadora.java
@@ -3,18 +3,18 @@ package miPrincipal;
 public class Calculadora {
 
     public double suma(double x, double y) {
-        return x + y;
+        return 0;
     }
     
     public double resta(double x, double y) {
-        return x - y;
+        return 0;
     }
 
     public double multiplica(double x, double y) {
-        return x * y;
+        return 0;
     }
 
     public double divide(double x, double y) {
-        return x / y;
+        return 0;
     }
 }
